23e34ee209696e4d63da91099e575b7d4b883825
hhant-max <sofia.sun.apply@outlook.com>
2022-11-12 12:56:36 +0100

    generate graph

--- Expriment/genGraph.py
+++ Expriment/genGraph.py
@@ -2,48 +2,74 @@ import numpy as np
 import networkx as nx
 from networkx.generators.community import (
     LFR_benchmark_graph,
-    planted_partition_graph,
     random_partition_graph,
 )
 from networkx.algorithms import community
-import os
-import math
-import functools
-
-# number of nodes n1
-n1 = 50
-k = 10
-p = 0.5
-mu = 0.5
+from argparse import ArgumentParser
 
 
 def get_q(mu, p, k):
     return float((mu * p) / ((1 - mu) * (k - 1)))
 
 
-def generate_graph(n1=50, k=10, p=0.5, mu=0.3):
+def generate_graph(graph, mu):
     # generate undirected unweighted graph
-    # G_P = planted_partition_graph(l=k, k=n1, p_in=p, p_out=get_q(mu, p, k), seed=0)
-    G_P = random_partition_graph(
-        sizes=[50 for _ in range(10)], p_in=p, p_out=get_q(mu, p, k), seed=0
-    )
-    print(f"Generated the planted partition graph with size: {len(G_P)}")
-    return G_P
+    if graph == "planted":
+        # number of nodes n1
+        n = 50
+        k = 10
+        p = 0.5
+        mu = mu
+
+        G = random_partition_graph(
+            sizes=[n for _ in range(k)], p_in=p, p_out=get_q(mu, p, k), seed=0
+        )
+
+    if graph == "LFR":
+        G = LFR_benchmark_graph()
+    # TODO: real datasets
+
+    print(f"Generated the planted partition graph with size: {len(G)}")
+    return G
 
     # test the first partion one
     # one_commu = G_P.graph["partition"][0]
 
-def output_graph(G_P):
+
+def output_graph(G_P, graph):
     # ouput the graph
     with open("Graph.txt", "wb") as f:
         nx.write_edgelist(G_P, f, data=False)
 
     # output the
     with open("TruthComms.txt", "w") as f:
-        for comm in G_P.graph["partition"]:
-            f.writelines(" ".join(list(map(lambda x: str(x), comm))))
-            f.write("\n")
+        if graph == "planted":
+            for comm in G_P.graph["partition"]:
+                f.writelines(" ".join(list(map(lambda x: str(x), comm))))
+                f.write("\n")
 
-print("#####################Generate graph############")
-G_P = generate_graph(n1=50, k=10, p=0.5, mu=0.3)
-output_graph(G_P)
+
+if __name__ == "__main__":
+
+    # parse the argument
+    description = """
+    """
+    epilog = """
+
+    """
+    parser = ArgumentParser(prog="genGraph", description=description, epilog=epilog)
+
+    # TODO: add real world datasets here
+    parser.add_argument(
+        "-g", metavar="graph name or dataset name", choices=["planted", "LFR"]
+    )
+    parser.add_argument("-mu", metavar="mu in planted graph", type=float)
+
+    args = parser.parse_args()
+    graph = args.g
+    mu = args.mu
+
+    #####################Generate graph############
+
+    G_P = generate_graph(graph=graph, mu=mu)
+    output_graph(G_P, graph=graph)
