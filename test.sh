512ba2664c0485dc163c5257eceef4b0827bb32d
hhant-max <sofia.sun.apply@outlook.com>
2022-11-17 11:05:28 +0100

    !change the motif type

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ####################generate graph
-# python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/genGraph.py -g 'planted' -mu 0.5
+python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/genGraph.py -g 'planted' -mu 0.5
 python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/genGraph.py -g 'LFR' -mu 0.4
 # python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/LFR_graph.txt -g 'LFR' -mu 0.4
 
@@ -11,6 +11,6 @@ cd ./project/localmotif/localmotifcluster || exit
 rm -rf outputComu.txt
 # ./experimentEvidence -d:Y -i:C-elegans-frontal.txt -m:FFLoop -s:1
 #######################get score
-./experimentEvidence -d:false -i:/home/sfy/Documents/VScodeProject/SNAPro/Graph.txt -m:UEdge
+./experimentEvidence -d:false -i:/home/sfy/Documents/VScodeProject/SNAPro/Graph.txt -m:clique3
 
 /home/sfy/miniconda3/envs/sna/bin/python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/getScore.py
