aa2d3398f58598ad02a4b1da19bfdd217d7e793f
hhant-max <sofia.sun.apply@outlook.com>
2022-11-18 22:00:49 +0100

    s

--- test_real.sh
+++ test_real.sh
@@ -6,3 +6,5 @@ rm -rf outputComu.txt
 ./project/localmotif/localmotifcluster/realGraph -d:false -i:/home/sfy/Documents/VScodeProject/SNAPro/dataset/com-lj.ungraph.txt -m:clique3
 
 /home/sfy/miniconda3/envs/sna/bin/python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/getScore.py
+
+/Users/sunfeiyang/miniconda3/envs/sna/bin/python
