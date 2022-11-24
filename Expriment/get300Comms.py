84eff414dff8c94af1baac8b74dfdf961e623dea
hhant-max <sofia.sun.apply@outlook.com>
2022-11-24 11:42:56 +0100

    comm

--- Expriment/get300Comms.py
+++ Expriment/get300Comms.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 
 # ╰─➤  /home/sfy/miniconda3/envs/sna/bin/python /home/sfy/Documents/VScodeProject/SNAPro/Expriment/get100Comms.py -file '/home/sfy/Documents/VScodeProject/SNAPro/dataset/com-dblp.ungraph.txt'
 
-parser = ArgumentParser(prog="gen1000Comms")
+parser = ArgumentParser(prog="gen300Comms")
 parser.add_argument("-i", metavar="file name of original comms", type=str)
 args = parser.parse_args()
 # print(args.file)
@@ -15,6 +15,6 @@ with open(args.i, "r") as myfile:
     head = list(islice(myfile, 300))
 
 # always remember, use files in a with statement
-with open(os.path.join(os.getcwd(), "testWIKI.txt"), "w") as f2:
+with open(os.path.join(os.getcwd(), "TruthComms.txt"), "w") as f2:
     for item in head:
         f2.write(item)
