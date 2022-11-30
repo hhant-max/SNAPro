921f38e55296dd87c9a71a94aabd32a54ded51d5
hhant-max <sofia.sun.apply@outlook.com>
2022-11-30 20:17:57 +0100

    plot

--- Expriment/plot.py
+++ Expriment/plot.py
@@ -14,12 +14,12 @@ from matplotlib import pyplot as plt
 #     .split("Starting mu : 0.1")
 # )
 
-figName = "test.png"
+# figName = "test.png"
 
 # create result of planted graph # clique3
-mu = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-alNamesM = ["MAPPR" for _ in range(10)]
-alNamesA = ["APPR" for _ in range(10)]
+mu = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+alNamesM = ["MAPPR" for _ in range(9)]
+alNamesA = ["APPR" for _ in range(9)]
 
 PF1scoreA = [1.0, 1.0, 0.973, 0.458, 0.337, 0.337, 0.336, 0.326, 0.221]
 PF1scoreM = [1.0, 1.0, 1.0, 1.0, 1.0, 0.959, 0.338, 0.333, 0.220]
@@ -28,24 +28,36 @@ PtimeA = [1.1, 1.5, 1.9, 2.1, 2.5, 3.0, 4.4, 6.5, 11.2]
 
 # create result of LFR # clique3
 LF1scoreA = [
-    0.9690543190560558,
-    0.3993207861247067,
-    0.33681561366125123,
-    0.33613767820070706,
-    0.3253332962958847,
+    1.0,
+    0.984,
+    0.726,
+    0.142,
+    0.122,
+    0.116,
+    0.094,
+    0.078,
+    0.082
 ]
-LF1scoreM = [1.0, 1.0, 0.1, 0.1, 0.958, 0.338, 0.333, 0.220]
-LtimeM = [0.73, 1.3, 1.6, 2.05, 2.12, 2.98, 4.41, 6.72, 11.1]
-LtimeA = []
+LF1scoreM = [1.0, 1.0, 0.998, 0.978, 0.739, 0.303, 0.106, 0.085,0.083]
+LtimeM = [1.2, 3.1, 3.4, 3.4, 3.5, 3.4, 3.5, 3.5, 3.3]
+LtimeA = [5.0,5.5,5.5,5.7,6.0,6.1,6.4,6.0,6.0]
 
+# result = {
+#     "mu": mu,
+#     "Algorithms": alNamesM + alNamesA,
+#     "F1score": PF1scoreM + PF1scoreA,
+#     # "Time(mins)": PtimeM + PtimeA
+# }
 
 result = {
     "mu": mu,
-    "algorithms": alNamesM + alNamesA,
-    "F1score": F1scoreM + F1scoreA,
+    "Algorithms": alNamesM + alNamesA,
+    # "F1score": LF1scoreM + LF1scoreA,
+    "Time(mins)": LtimeM + LtimeA
 }
 
-resultDF = pd.DataFrame(result, columns=["mu", "algorithms", "F1score"])
+
+resultDF = pd.DataFrame(result, columns=["mu", "Algorithms", "Time(mins)"])
 
 # plot
 # draw first and then add it
@@ -53,11 +65,11 @@ sns.set_theme(style="ticks")
 sns.lineplot(
     data=resultDF,
     x="mu",
-    y="F1score",
-    hue="algorithms",
-    style="algorithms",
+    y="Time(mins)",
+    hue="Algorithms",
+    style="Algorithms",
     markers=True,
     dashes=False,
 )
-
-plt.savefig(figName)
+# plt.show()
+plt.savefig("LFR_time.png")
